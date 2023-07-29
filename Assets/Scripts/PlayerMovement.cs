a4444a875800b08af279bdf806591a250e99d7f5
JustW <vuav2003@gmail.com>
2023-07-29 11:25:25 +0700

    multy scenes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
+    //jump
     [SerializeField] private float jumpForce;
-    [SerializeField] private LayerMask jumpableGround;
+    [SerializeField] private LayerMask groundLayer;
+    private bool canDoubleJump = false;
+
+    //wall
+    private bool isWallSliding;
+    [SerializeField] private float wallSlideSpeed;
+    [SerializeField] private Transform wallCheck;
+
 
     private Rigidbody2D rb;
     private Animator anim;
     private SpriteRenderer sprite;
     private BoxCollider2D coll;
     private float Horizontal = 0f;
+
+    
+
 
     private enum MovementState {idle, run, jump, fail }
     
@@ -28,12 +39,33 @@ public class PlayerMovement : MonoBehaviour
         Horizontal = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(Horizontal * moveSpeed, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        if (IsGrounded())
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            canDoubleJump = true;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (IsGrounded())
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                canDoubleJump = true; // Reset double jump ability when grounded
+            }
+            else if (canDoubleJump) // Perform double jump
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                canDoubleJump = false;
+            }
+            else if (isWallSliding) // Perform wall jump
+            {
+                float wallJumpDirection = IsWalled() ? -Mathf.Sign(Horizontal) : 0f;
+                rb.velocity = new Vector2(wallJumpDirection * jumpForce, jumpForce);
+                isWallSliding = false; // Exit wall slide after wall jump
+            }
         }
 
         UpdateAnimations();
+        WallSlide();
     }
     private void UpdateAnimations()
     {
@@ -64,6 +96,24 @@ public class PlayerMovement : MonoBehaviour
     }
     private bool IsGrounded()
     {
-        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround); //if player not on ground player can't jump
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, groundLayer); 
+    }
+
+    private bool IsWalled()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, groundLayer);
+    }
+
+    private void WallSlide()
+    {
+        if(IsWalled() && !IsGrounded() && Horizontal !=0f ) 
+        { 
+            isWallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlideSpeed, float.MaxValue));
+        }
+        else 
+        { 
+            isWallSliding = false;
+        }
     }
 }
---
+++ Assets/Scripts/checkPoint.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class checkPoints : MonoBehaviour
+{
+    [SerializeField] private float delayTime;
+    [SerializeField] private string nameScene;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.LogWarning("abcaska");
+            ModeSelect();
+        }
+    }
+    public void ModeSelect()
+    {
+        StartCoroutine(LoadAfterDelay());
+    }
+
+    IEnumerator LoadAfterDelay()
+    {
+        yield return new WaitForSeconds(delayTime);
+
+        SceneManager.LoadScene(nameScene);
+    }
+}
